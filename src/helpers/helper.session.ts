79af13129ba1a904bb924c3b5d23b22e66a84ca2
restuwahyu47 <restuwahyu@pazemo.com>
2023-03-27 01:24:48 +0700

    Publish files

--- src/helpers/helper.session.ts
+++ src/helpers/helper.session.ts
@@ -5,6 +5,7 @@ export const session = async (prefix: string): Promise<boolean> => {
   const redis: InstanceType<typeof Redis> = new Redis(0)
   const sessions: InstanceType<typeof Sessions> = new Sessions()
   const sessionLimit: number = +process.env.SESSION_LOGIN_LIMIT
+  const sessionExpired: number = +process.env.JWT_EXPIRED
 
   const redisKey: string[] = [`${prefix}-session`, `${prefix}-token`, `${prefix}-signatures`, 'secretkey', 'signature']
   const sessionKey: number = +(await redis.keyCacheDataExist(`${prefix}-session`))
@@ -13,6 +14,7 @@ export const session = async (prefix: string): Promise<boolean> => {
   else if (sessionKey) {
     const sessionInc: number = +(await redis.config().llen(`${prefix}-session`))
     await redis.config().lpush(`${prefix}-session`, sessionInc + 1)
+    await redis.config().expire(`${prefix}-session`, sessionExpired)
 
     if (sessionInc >= sessionLimit) {
       await sessions.model().delete().where({ user_id: prefix, type: 'login' })
